02110498a9c60c3ceb7336e7bc27002d1d706232
dimitri-dev <maric.neven03@gmail.com>
2023-12-11 23:03:34 +0100

    AOC 2023 - day 11 - part B

---
+++ 2023/Code/Day_011/PartB.cs
@@ -0,0 +1,11 @@
+﻿namespace Code.Day_011;
+
+public partial class CosmicExpansion
+{
+    public long Solve_PartB(string inputPath = "Inputs/011.in")
+    {
+        _map = File.ReadAllLines(inputPath).ToList();
+        
+        return GetOldGalaxies(1_000_000);
+    }
+}
