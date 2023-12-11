59092faf86dd515775304c8ad36c6ff2c46bb7e3
dimitri-dev <maric.neven03@gmail.com>
2023-12-11 23:03:24 +0100

    AOC 2023 - day 11 - part A

---
+++ 2023/Code/Day_011/PartA.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+
+namespace Code.Day_011;
+
+public partial class CosmicExpansion
+{
+    private List<string> _map;
+    
+    private long GetOldGalaxies(int expansionRate)
+    {
+        var emptyRows = _map.Select((row, idx) => (row, idx))
+            .Where(enumerated => enumerated.row.All(ch => ch == '.'))
+            .Select(x => x.idx)
+            .ToList();
+        
+        var emptyCols = _map.Select((row, idx) => (row, idx))
+            .Where(enumerated => _map.All(row => row[enumerated.idx] == '.'))
+            .Select(x => x.idx)
+            .ToList();
+
+        var galaxies = _map.SelectMany((row, x) =>
+        {
+            return row.Select((ch, y) => (ch, y))
+                .Where(enumeratedRow => enumeratedRow.ch == '#')
+                .Select(enumeratedRow => new Point(x, enumeratedRow.y));
+        }).ToList();
+
+        long sum = galaxies.SelectMany((left, idx) =>
+        {
+            return galaxies.Skip(idx + 1).Select(right =>
+            {
+                long distanceSum = Math.Abs(left.x - right.x) + Math.Abs(left.y - right.y);
+
+                long extraRows = (expansionRate - 1) *
+                                 emptyRows.Count(rowIdx =>
+                                     (left.x < rowIdx && right.x > rowIdx) || (left.x > rowIdx && right.x < rowIdx));
+
+                long extraColumns = (expansionRate - 1) *
+                                    emptyCols.Count(colIdx =>
+                                        (left.y < colIdx && right.y > colIdx) || (left.y > colIdx && right.y < colIdx));
+
+                return distanceSum + extraRows + extraColumns;
+            });
+        }).Sum();
+
+        return sum;
+    }
+    
+    public long Solve_PartA(string inputPath = "Inputs/011.in")
+    {
+        _map = File.ReadAllLines(inputPath).ToList();
+        
+        return GetOldGalaxies(2);
+    }
+
+    private record Point(int x, int y);
+}
--- 2023/Console/Program.cs
+++ 2023/Console/Program.cs
@@ -1,5 +1,5 @@
-﻿var instance = new Code.Day_010.PipeMaze();
+﻿var instance = new Code.Day_011.CosmicExpansion();
 
-Console.WriteLine($"Part A: {instance.Solve_PartA("Inputs/010.in")}");
+Console.WriteLine($"Part A: {instance.Solve_PartA("Inputs/011.in")}");
 
-Console.WriteLine($"Part B: {instance.Solve_PartB("Inputs/010.in")}");
+Console.WriteLine($"Part B: {instance.Solve_PartB("Inputs/011.in")}");
