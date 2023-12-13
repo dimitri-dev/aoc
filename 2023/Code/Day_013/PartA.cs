cd7fce9b2e6271c99433a2567a880de159b0bbba
dimitri-dev <maric.neven03@gmail.com>
2023-12-13 07:54:32 +0100

    AOC 2023 - day 13 - part A

---
+++ 2023/Code/Day_013/PartA.cs
@@ -0,0 +1,40 @@
+﻿namespace Code.Day_013;
+
+public partial class PointOfIncidence
+{
+    public long FindMirror(string _map)
+    {
+        var _mapHorizontal = _map.Split('\n').ToArray();
+        
+        var _mapVertical = Enumerable.Range(0, _mapHorizontal[0].Length)
+            .Select(i => new string(_mapHorizontal.Select(row => row[i]).ToArray()))
+            .ToArray();
+
+        var values = new[] { (_mapHorizontal, 100), (_mapVertical, 1) };
+
+        foreach ((string[] map, int mapWeight) in values)
+        {
+            for (int i = 1; i < map.Length; ++i)
+            {
+                var a = string.Join("", map.Take(i).Reverse());
+                var b = string.Join("", map.Skip(i));
+
+                if (a.StartsWith(b) || b.StartsWith(a))
+                {
+                    return  i * mapWeight;
+                }
+            }
+        }
+
+        return -1;
+    }
+    
+    public long Solve_PartA(string inputPath = "Inputs/013.in")
+    {
+        var maps = string.Join("\n", File.ReadAllLines(inputPath).ToArray()).Split("\n\n");
+
+        long result = maps.Sum(x => FindMirror(x));
+        
+        return result;
+    }
+}
--- 2023/Console/Program.cs
+++ 2023/Console/Program.cs
@@ -1,5 +1,5 @@
-﻿var instance = new Code.Day_012.HotSprings();
+﻿var instance = new Code.Day_013.PointOfIncidence();
 
-Console.WriteLine($"Part A: {instance.Solve_PartA("Inputs/012.in")}");
+Console.WriteLine($"Part A: {instance.Solve_PartA("Inputs/013.in")}");
 
-Console.WriteLine($"Part B: {instance.Solve_PartB("Inputs/012.in")}");
+Console.WriteLine($"Part B: {instance.Solve_PartB("Inputs/013.in")}");
