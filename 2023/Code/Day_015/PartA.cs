3dae53466a8411b77c10ad0c1e9cbce5f992a266
Neven Marić <neven.maric@moberg.hr>
2023-12-15 12:55:50 +0100

    AOC 2023 - day 15 - part A

---
+++ 2023/Code/Day_015/PartA.cs
@@ -0,0 +1,27 @@
+﻿namespace Code.Day_015;
+
+public partial class LensLibrary
+{
+    public int GetHashAlgorithmResult(string line)
+    {
+        var currentValue = 0;
+
+        foreach (var ch in line)
+        {
+            currentValue += (int)ch;
+            currentValue *= 17;
+            currentValue %= 256;
+        }
+
+        return currentValue;
+    }
+
+    public long Solve_PartA(string inputPath = "Inputs/015.in")
+    {
+        var lines = File.ReadAllLines(inputPath).First().Split(',');
+
+        var result = lines.Sum(GetHashAlgorithmResult);
+
+        return result;
+    }
+}
--- 2023/Console/Program.cs
+++ 2023/Console/Program.cs
@@ -1,5 +1,5 @@
-﻿var instance = new Code.Day_014.ParabolicReflectorDish();
+﻿var instance = new Code.Day_015.LensLibrary();
 
-Console.WriteLine($"Part A: {instance.Solve_PartA("Inputs/014.in")}");
+Console.WriteLine($"Part A: {instance.Solve_PartA()}");
 
-Console.WriteLine($"Part B: {instance.Solve_PartB("Inputs/014.in")}");
+Console.WriteLine($"Part B: {instance.Solve_PartB("Inputs/015.in")}");
