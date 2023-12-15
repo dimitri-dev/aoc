0e7161455c8c5e8eb597720dbffba4c8c0530424
dimitri-dev <maric.neven03@gmail.com>
2023-12-15 07:56:32 +0100

    AOC 2023 - day 14 - part A

---
+++ 2023/Code/Day_014/PartA.cs
@@ -0,0 +1,59 @@
+﻿namespace Code.Day_014;
+
+public partial class ParabolicReflectorDish
+{
+    public long GetTotalLoad(char[,] map, int size)
+    {
+        long load = 0;
+        for (int r = 0; r < size; r++) 
+        {
+            for (int c = 0; c < size; c++) 
+            {
+                if (map[r, c] == 'O')
+                    load += size - r;
+            }
+        }
+
+        return load;
+    }
+
+    public void RollNorth(char[,] map, int size)
+    {
+        for (int r = 1; r < size; r++)
+        {
+            for (int c = 0; c < size; c++)
+            {
+                if (map[r, c] == 'O')
+                {
+                    int nr = r;
+                    while (nr - 1 >= 0 && map[nr - 1, c] == '.') 
+                    {
+                        map[nr - 1, c] = 'O';
+                        map[nr, c] = '.';
+                        --nr;
+                    }
+                }
+            }
+        }
+    }
+    
+    public long Solve_PartA(string inputPath = "Inputs/014.in")
+    {
+        var lines = File.ReadAllLines(inputPath);
+        int size = lines.Length;
+        
+        char[,] map = new char[lines.Length, lines.Length];
+        for (int r = 0; r < lines.Length; r++) 
+        {
+            for (int c = 0; c < lines.Length; c++) 
+            {
+                map[r, c] = lines[r][c];
+            }
+        }
+        
+        RollNorth(map, lines.Length);
+        var result = GetTotalLoad(map, size);
+        
+        return result;
+    }
+}
--- 2023/Console/Program.cs
+++ 2023/Console/Program.cs
@@ -1,5 +1,5 @@
-﻿var instance = new Code.Day_013.PointOfIncidence();
+﻿var instance = new Code.Day_014.ParabolicReflectorDish();
 
-Console.WriteLine($"Part A: {instance.Solve_PartA("Inputs/013.in")}");
+Console.WriteLine($"Part A: {instance.Solve_PartA("Inputs/014.in")}");
 
-Console.WriteLine($"Part B: {instance.Solve_PartB("Inputs/013.in")}");
+Console.WriteLine($"Part B: {instance.Solve_PartB("Inputs/014.in")}");
